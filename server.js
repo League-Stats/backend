dfa9264d6dcdd69fef65ed2e346f6f385f00afe8
JaySebagh <jaysebagh@gmail.com>
2019-12-26 15:55:16 -0800

    feat(rank + history): retrieved summoner's ranks and match history

--- server.js
+++ server.js
@@ -18,6 +18,9 @@ server.get('/', (req, res) => {
     res.send('alive')
 });
 
+let currentRegionApi;
+let summonerAccount = {};
+
 server.get('/accountInfo', async (req, res) => {
     const summonerName = req.body.summonerName;
     const summonerRegion = req.body.summonerRegion;
@@ -32,7 +35,7 @@ server.get('/accountInfo', async (req, res) => {
         return
     };
 
-    const currentRegionApi = regions[summonerRegion];
+    currentRegionApi = regions[summonerRegion];
 
     try{
         const accountResponse = await axios.get(`${currentRegionApi}/lol/summoner/v4/summoners/by-name/${summonerName}`, {
@@ -40,10 +43,64 @@ server.get('/accountInfo', async (req, res) => {
                 "X-Riot-Token": RIOT_TOKEN
             }
         });
-        res.status(200).json(accountResponse.data);
+        summonerAccount.id = accountResponse.data.id;
+        summonerAccount.accountId = accountResponse.data.accountId;
+        summonerAccount.name = accountResponse.data.name;
+        summonerAccount.profileIconId = accountResponse.data.profileIconId;
+        summonerAccount.summonerLevel = accountResponse.data.summonerLevel;
+        res.status(200).json(summonerAccount);
     } catch(error) {
-        res.status(400).json(error)
+        res.status(400).json(error);
         return
+    }
+})
+
+server.get('/rank', async (req, res) => {
+    if(!summonerAccount.id || !summonerAccount.accountId){
+        res.status(400).json({ message: "Missing AccountID or ID"})
+    }
+
+    let summonerRank = [];
+
+    try{
+        const accountResponse = await axios.get(`${currentRegionApi}/lol/league/v4/entries/by-summoner/${summonerAccount.id}`, {
+            headers: {
+                "X-Riot-Token": RIOT_TOKEN
+            }
+        });
+        accountResponse.data.forEach(queue => {
+            summonerRank.push({
+                queueType: queue.queueType,
+                tier: queue.tier,
+                rank: queue.rank,
+                leaguePoints: queue.leaguePoints,
+                wins: queue.wins,
+                losses: queue.losses
+            })
+        })
+        res.status(200).json(summonerRank);
+    } catch(error) {
+        res.status(400).json(error);
+    }
+})
+
+server.get('/matchhistory', async (req, res) => {
+    if(!summonerAccount.id || !summonerAccount.accountId){
+        res.status(400).json({ message: "Missing AccountID or ID"})
+    }
+
+    let summonerMatchHistory = [];
+
+    try{
+        const accountResponse = await axios.get(`${currentRegionApi}/lol/match/v4/matchlists/by-account/${summonerAccount.accountId}`, {
+            headers: {
+                "X-Riot-Token": RIOT_TOKEN
+            }
+        });
+        console.log(accountResponse.data.matches)
+        res.status(200).json(accountResponse);
+    } catch(error) {
+        res.status(400).json(error);
     }
 })
 
