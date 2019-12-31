2a4364401fbb46f2839579a2f5f8805ef825b6e3
JaySebagh <jaysebagh@gmail.com>
2019-12-30 16:55:14 -0800

    feat(match details): parsed data as well as added destructuring to other endpoints

--- server.js
+++ server.js
@@ -38,16 +38,20 @@ server.get('/accountInfo', async (req, res) => {
     currentRegionApi = regions[summonerRegion];
 
     try{
-        const accountResponse = await axios.get(`${currentRegionApi}/lol/summoner/v4/summoners/by-name/${summonerName}`, {
+        const { data } = await axios.get(`${currentRegionApi}/lol/summoner/v4/summoners/by-name/${summonerName}`, {
             headers: {
                 "X-Riot-Token": RIOT_TOKEN
             }
         });
-        summonerAccount.id = accountResponse.data.id;
-        summonerAccount.accountId = accountResponse.data.accountId;
-        summonerAccount.name = accountResponse.data.name;
-        summonerAccount.profileIconId = accountResponse.data.profileIconId;
-        summonerAccount.summonerLevel = accountResponse.data.summonerLevel;
+
+        summonerAccount = {
+            id: data.id,
+            accountId: data.accountId,
+            name: data.name,
+            profileIconId: data.profileIconId,
+            summonerLevel: data.summonerLevel
+        }
+
         res.status(200).json(summonerAccount);
     } catch(error) {
         res.status(400).json(error);
@@ -63,12 +67,13 @@ server.get('/rank', async (req, res) => {
     let summonerRank = [];
 
     try{
-        const accountResponse = await axios.get(`${currentRegionApi}/lol/league/v4/entries/by-summoner/${summonerAccount.id}`, {
+        const { data } = await axios.get(`${currentRegionApi}/lol/league/v4/entries/by-summoner/${summonerAccount.id}`, {
             headers: {
                 "X-Riot-Token": RIOT_TOKEN
             }
         });
-        accountResponse.data.forEach(queue => {
+
+        data.forEach(queue => {
             summonerRank.push({
                 queueType: queue.queueType,
                 tier: queue.tier,
@@ -78,6 +83,7 @@ server.get('/rank', async (req, res) => {
                 losses: queue.losses
             })
         })
+
         res.status(200).json(summonerRank);
     } catch(error) {
         res.status(400).json(error);
@@ -90,12 +96,12 @@ server.get('/matchhistory', async (req, res) => {
     }
 
     try{
-        const accountResponse = await axios.get(`${currentRegionApi}/lol/match/v4/matchlists/by-account/${summonerAccount.accountId}`, {
+        const { data } = await axios.get(`${currentRegionApi}/lol/match/v4/matchlists/by-account/${summonerAccount.accountId}`, {
             headers: {
                 "X-Riot-Token": RIOT_TOKEN
             }
         });
-        res.status(200).json(accountResponse.data.matches);
+        res.status(200).json(data.matches);
     } catch(error) {
         res.status(400).json(error);
     }
@@ -110,14 +116,88 @@ server.get('/matchdetails', async (req, res) => {
     };
 
     try{
-        const accountResponse = await axios.get(`${currentRegionApi}/lol/match/v4/matches/${matchId}`, {
+        const { data } = await axios.get(`${currentRegionApi}/lol/match/v4/matches/${matchId}`, {
             headers: {
                 "X-Riot-Token": RIOT_TOKEN
             }
         });
-        console.log(accountResponse.data)
-        res.status(200).json(accountResponse.data);
+
+        let matchData = {
+            queueId: data.queueId,
+            participantIdentities: data.participantIdentities.map(smnr => {
+                return {
+                    player: {
+                        summonerName: smnr.player.summonerName,
+                        currentAccountId: smnr.player.currentAccountId,
+                        profileIcon: smnr.player.profileIcon,
+                        summonerId: smnr.player.summonerId
+                    },
+                    participantId: smnr.participantId
+                }
+            }),
+            gameMode: data.gameMode,
+            mapId: data.mapId,
+            gameType: data.gameType,
+            teams: data.teams.map(team => {
+                return {
+                    win: team.win,
+                    baronKills: team.baronKills,
+                    riftHeraldKills: team.riftHeraldKills,
+                    teamId: team.teamId,
+                    inhibitorKills: team.inhibitorKills,
+                    towerKills: team.towerKills,
+                    dragonKills: team.dragonKills
+                }
+            }),
+            participants: data.participants.map(smnr => {
+                return {
+                    spell1Id: smnr.spell1Id,
+                    participantId: smnr.participantId,
+                    timeline: {
+                        lane: smnr.timeline.lane,
+                        participantId: smnr.timeline.participantId
+                    },
+                    spell2Id: smnr.spell2Id,
+                    teamId: smnr.teamId,
+                    stats: {
+                        magicDamageDealtToChampions: smnr.stats.magicDamageDealtToChampions,
+                        tripleKills: smnr.stats.tripleKills,
+                        kills: smnr.stats.kills,
+                        wardsKilled: smnr.stats.wardsKilled,
+                        quadraKills: smnr.stats.quadraKills,
+                        item2: smnr.stats.item2,
+                        item3: smnr.stats.item3,
+                        item0: smnr.stats.item0,
+                        item1: smnr.stats.item1,
+                        item6: smnr.stats.item6,
+                        item4: smnr.stats.item4,
+                        item5: smnr.stats.item5,
+                        assists: smnr.stats.assists,
+                        participantId: smnr.stats.participantId,
+                        sightWardsBoughtInGame: smnr.stats.sightWardsBoughtInGame,
+                        totalDamageDealtToChampions: smnr.stats.totalDamageDealtToChampions,
+                        win: smnr.stats.win,
+                        totalDamageDealt: smnr.stats.totalDamageDealt,
+                        deaths: smnr.stats.deaths,
+                        wardsPlaced: smnr.stats.wardsPlaced,
+                        perkPrimaryStyle: smnr.stats.perkPrimaryStyle,
+                        perkSubStyle: smnr.stats.perkSubStyle,
+                        goldEarned: smnr.stats.goldEarned,
+                        champLevel: smnr.stats.champLevel,
+                        doubleKills: smnr.stats.doubleKills,
+                        visionWardsBoughtInGame: smnr.stats.visionWardsBoughtInGame,
+                        pentaKills: smnr.stats.pentaKills,
+                        totalMinionsKilled: smnr.stats.totalMinionsKilled,
+                    },
+                    championId: smnr.championId
+                }
+            }),
+            gameDuration: data.gameDuration,
+            gameCreation: data.gameCreation
+        }
+        res.status(200).json(matchData);
     } catch(error) {
+        console.log(error)
         res.status(400).json(error);
         return
     }
