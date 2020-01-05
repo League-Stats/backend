a8c9f38c1536b427c7c192cd588bca83d350bfb9
JaySebagh <jaysebagh@gmail.com>
2020-01-05 15:57:51 -0800

    completed favorites functionality

--- users/favorites/favorites-model.js
+++ users/favorites/favorites-model.js
@@ -1,18 +1,25 @@
 const db = require('../../data/dbConfig');
 
 module.exports = {
-    // add,
-    // remove,
+    add,
+    remove,
     viewAll
 }
 
-// async function add(name, region){
+async function add(name, region, user_id){
+    return db('favorites')
+        .insert({
+            name: name,
+            region: region,
+            user_id: user_id
+        })
+}
 
-// }
-
-// async function remove(name, region){
-// // call viewAll after removing
-// }
+async function remove(id, user_id){
+    return db('favorites')
+        .where({ id, user_id })
+        .del()
+}
 
 function viewAll(user_id, region){
     return db('favorites')
--- users/favorites/favorites-router.js
+++ users/favorites/favorites-router.js
@@ -5,14 +5,35 @@ const restricted = require('../../auth/restricted-middleware');
 
 router.get('/favorites', restricted, async (req, res) => {
     try{
-        // const user_id =
-        console.log(currentUser)
         const region = req.body.summonerRegion;
-        const added = await Fav.viewAll(user_id)
-        res.status(200).json({ added })
+        const added = await Fav.viewAll(req.decodedToken.subject, region);
+        res.status(200).json({ added });
     }catch (error){
         console.log(error);
         res.status(500).json({ message: "failed to retrieve favorites" });
+    }
+})
+
+router.post('/favorites', restricted, async (req, res) => {
+    try{
+        const name = req.body.summonerName;
+        const region = req.body.summonerRegion;
+        const added = await Fav.add(name, region, req.decodedToken.subject);
+        res.status(200).json({ added, message: `${name} added to ${region} region` });
+    }catch(error){
+        console.log(error);
+        res.status(500).json({ message: "failed to add summoner to favorites" });
+    }
+})
+
+router.delete('/favorites', restricted, async (req, res) => {
+    try{
+        const id = req.body.favoritesId
+        const removed = await Fav.remove(id, req.decodedToken.subject)
+        res.status(200).json({ removed, message: `Removed from favorites` });
+    }catch(error){
+        console.log(error);
+        res.status(500).json({ message: "failed to delete summoner from favorites" });
     }
 })
 
