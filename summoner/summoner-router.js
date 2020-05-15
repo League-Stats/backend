03e078d095604661ecc755baa95176176e8eb7f6
JaySebagh <jaysebagh@gmail.com>
2020-05-15 15:36:14 -0700

    Refactored matchDetails API endpoint for easier frontend management

--- summoner/summoner-router.js
+++ summoner/summoner-router.js
@@ -187,19 +187,147 @@ router.post('/matchdetails', async (req, res) => {
             }
         });
 
-        let matchData = {
-            queueId: data.queueId,
-            participantIdentities: data.participantIdentities.map(smnr => {
-                return {
-                    player: {
-                        summonerName: smnr.player.summonerName,
-                        currentAccountId: smnr.player.currentAccountId,
-                        profileIcon: smnr.player.profileIcon,
-                        summonerId: smnr.player.summonerId
-                    },
+
+        // let matchData = {
+        //     queueId: data.queueId,
+        //     participantIdentities: data.participantIdentities.map(smnr => {
+        //         return {
+        //             player: {
+        //                 summonerName: smnr.player.summonerName,
+        //                 currentAccountId: smnr.player.currentAccountId,
+        //                 profileIcon: smnr.player.profileIcon,
+        //                 summonerId: smnr.player.summonerId
+        //             },
+        //             participantId: smnr.participantId
+        //         }
+        //     }),
+        //     gameMode: data.gameMode,
+        //     mapId: data.mapId,
+        //     gameType: data.gameType,
+        //     teams: data.teams.map(team => {
+        //         return {
+        //             win: team.win,
+        //             baronKills: team.baronKills,
+        //             riftHeraldKills: team.riftHeraldKills,
+        //             teamId: team.teamId,
+        //             inhibitorKills: team.inhibitorKills,
+        //             towerKills: team.towerKills,
+        //             dragonKills: team.dragonKills
+        //         }
+        //     }),
+        //     participants: data.participants.map(smnr => {
+        //         return {
+        //             spell1Id: smnr.spell1Id,
+        //             participantId: smnr.participantId,
+        //             timeline: {
+        //                 lane: smnr.timeline.lane,
+        //                 participantId: smnr.timeline.participantId
+        //             },
+        //             spell2Id: smnr.spell2Id,
+        //             teamId: smnr.teamId,
+        //             stats: {
+        //                 magicDamageDealtToChampions: smnr.stats.magicDamageDealtToChampions,
+        //                 tripleKills: smnr.stats.tripleKills,
+        //                 kills: smnr.stats.kills,
+        //                 wardsKilled: smnr.stats.wardsKilled,
+        //                 quadraKills: smnr.stats.quadraKills,
+        //                 item2: smnr.stats.item2,
+        //                 item3: smnr.stats.item3,
+        //                 item0: smnr.stats.item0,
+        //                 item1: smnr.stats.item1,
+        //                 item6: smnr.stats.item6,
+        //                 item4: smnr.stats.item4,
+        //                 item5: smnr.stats.item5,
+        //                 assists: smnr.stats.assists,
+        //                 participantId: smnr.stats.participantId,
+        //                 sightWardsBoughtInGame: smnr.stats.sightWardsBoughtInGame,
+        //                 totalDamageDealtToChampions: smnr.stats.totalDamageDealtToChampions,
+        //                 win: smnr.stats.win,
+        //                 totalDamageDealt: smnr.stats.totalDamageDealt,
+        //                 deaths: smnr.stats.deaths,
+        //                 wardsPlaced: smnr.stats.wardsPlaced,
+        //                 perkPrimaryStyle: smnr.stats.perkPrimaryStyle,
+        //                 perkSubStyle: smnr.stats.perkSubStyle,
+        //                 goldEarned: smnr.stats.goldEarned,
+        //                 champLevel: smnr.stats.champLevel,
+        //                 doubleKills: smnr.stats.doubleKills,
+        //                 visionWardsBoughtInGame: smnr.stats.visionWardsBoughtInGame,
+        //                 pentaKills: smnr.stats.pentaKills,
+        //                 totalMinionsKilled: smnr.stats.totalMinionsKilled,
+        //             },
+        //             championId: smnr.championId
+        //         }
+        //     }),
+        //     gameDuration: data.gameDuration,
+        //     gameCreation: data.gameCreation
+        // }
+
+        let participantIdentities = data.participantIdentities.map(smnr => {
+            return {
+                player: {
+                    summonerName: smnr.player.summonerName,
+                    currentAccountId: smnr.player.currentAccountId,
+                    profileIcon: smnr.player.profileIcon,
+                    summonerId: smnr.player.summonerId,
                     participantId: smnr.participantId
                 }
-            }),
+            }
+        })
+        
+        let participants = data.participants.map(smnr => {
+            return {
+                spell1Id: smnr.spell1Id,
+                participantId: smnr.participantId,
+                timeline: {
+                    lane: smnr.timeline.lane,
+                    participantId: smnr.timeline.participantId
+                },
+                spell2Id: smnr.spell2Id,
+                teamId: smnr.teamId,
+                stats: {
+                    magicDamageDealtToChampions: smnr.stats.magicDamageDealtToChampions,
+                    tripleKills: smnr.stats.tripleKills,
+                    kills: smnr.stats.kills,
+                    wardsKilled: smnr.stats.wardsKilled,
+                    quadraKills: smnr.stats.quadraKills,
+                    item2: smnr.stats.item2,
+                    item3: smnr.stats.item3,
+                    item0: smnr.stats.item0,
+                    item1: smnr.stats.item1,
+                    item6: smnr.stats.item6,
+                    item4: smnr.stats.item4,
+                    item5: smnr.stats.item5,
+                    assists: smnr.stats.assists,
+                    participantId: smnr.stats.participantId,
+                    sightWardsBoughtInGame: smnr.stats.sightWardsBoughtInGame,
+                    totalDamageDealtToChampions: smnr.stats.totalDamageDealtToChampions,
+                    win: smnr.stats.win,
+                    totalDamageDealt: smnr.stats.totalDamageDealt,
+                    deaths: smnr.stats.deaths,
+                    wardsPlaced: smnr.stats.wardsPlaced,
+                    perkPrimaryStyle: smnr.stats.perkPrimaryStyle,
+                    perkSubStyle: smnr.stats.perkSubStyle,
+                    goldEarned: smnr.stats.goldEarned,
+                    champLevel: smnr.stats.champLevel,
+                    doubleKills: smnr.stats.doubleKills,
+                    visionWardsBoughtInGame: smnr.stats.visionWardsBoughtInGame,
+                    pentaKills: smnr.stats.pentaKills,
+                    totalMinionsKilled: smnr.stats.totalMinionsKilled,
+                },
+                championId: smnr.championId
+            }
+        })
+
+        const mergeParticipantInfo = participantIdentities.map((item, i)=>{
+            if(item.player.participantId === participants[i].participantId){
+              return Object.assign({},item,participants[i])
+            }
+         })
+
+
+         let matchData = {
+            queueId: data.queueId,
+            participantsInfo: mergeParticipantInfo,
             gameMode: data.gameMode,
             mapId: data.mapId,
             gameType: data.gameType,
@@ -214,52 +342,10 @@ router.post('/matchdetails', async (req, res) => {
                     dragonKills: team.dragonKills
                 }
             }),
-            participants: data.participants.map(smnr => {
-                return {
-                    spell1Id: smnr.spell1Id,
-                    participantId: smnr.participantId,
-                    timeline: {
-                        lane: smnr.timeline.lane,
-                        participantId: smnr.timeline.participantId
-                    },
-                    spell2Id: smnr.spell2Id,
-                    teamId: smnr.teamId,
-                    stats: {
-                        magicDamageDealtToChampions: smnr.stats.magicDamageDealtToChampions,
-                        tripleKills: smnr.stats.tripleKills,
-                        kills: smnr.stats.kills,
-                        wardsKilled: smnr.stats.wardsKilled,
-                        quadraKills: smnr.stats.quadraKills,
-                        item2: smnr.stats.item2,
-                        item3: smnr.stats.item3,
-                        item0: smnr.stats.item0,
-                        item1: smnr.stats.item1,
-                        item6: smnr.stats.item6,
-                        item4: smnr.stats.item4,
-                        item5: smnr.stats.item5,
-                        assists: smnr.stats.assists,
-                        participantId: smnr.stats.participantId,
-                        sightWardsBoughtInGame: smnr.stats.sightWardsBoughtInGame,
-                        totalDamageDealtToChampions: smnr.stats.totalDamageDealtToChampions,
-                        win: smnr.stats.win,
-                        totalDamageDealt: smnr.stats.totalDamageDealt,
-                        deaths: smnr.stats.deaths,
-                        wardsPlaced: smnr.stats.wardsPlaced,
-                        perkPrimaryStyle: smnr.stats.perkPrimaryStyle,
-                        perkSubStyle: smnr.stats.perkSubStyle,
-                        goldEarned: smnr.stats.goldEarned,
-                        champLevel: smnr.stats.champLevel,
-                        doubleKills: smnr.stats.doubleKills,
-                        visionWardsBoughtInGame: smnr.stats.visionWardsBoughtInGame,
-                        pentaKills: smnr.stats.pentaKills,
-                        totalMinionsKilled: smnr.stats.totalMinionsKilled,
-                    },
-                    championId: smnr.championId
-                }
-            }),
             gameDuration: data.gameDuration,
             gameCreation: data.gameCreation
         }
+
         res.status(200).json(matchData);
     } catch(error) {
         console.log(error)
